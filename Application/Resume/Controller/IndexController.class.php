56a760ec17e15978e9dbcda74a135eca2f27b6b1
张玥 <zy514881101@gmail.com>
2018-02-25 21:47:07 +0800

    前端获取简历信息接口

--- Application/Resume/Controller/IndexController.class.php
+++ Application/Resume/Controller/IndexController.class.php
@@ -10,10 +10,10 @@
 // | 拷贝、复制、传播、使用零云的任意代码，如有违反，请立即删除，否则您将面临承担相应
 // | 法律责任的风险。如果需要取得官方授权，请联系官方http://www.lingyun.net
 // +----------------------------------------------------------------------
-namespace Block\Controller;
+namespace Resume\Controller;
 
 use Home\Controller\HomeController;
-use lyf\Page;
+
 
 /**
  * 默认控制器
@@ -22,43 +22,12 @@ use lyf\Page;
 class IndexController extends HomeController
 {
     /**
-     * 首页
+     * 前端获取简历信息接口
      * @author jry <598821125@qq.com>
      */
     public function index()
     {
-        $team_list = D('Team')->where(array('status' => 1))->limit(6)->order('id desc')->select();
-        $this->assign('team_list',$team_list);
-        $this->assign('meta_title', 'Home');
-        $this->display();
+        $info = D('Index')->get_full_info(1);
+        $this->success($info);
     }
-
-    // public function partners()
-    // {
-    // 	$this->assign('meta_title', 'Partners');
-    //     $this->display();
-    // }
-
-    public function progress()
-    {
-        $this->assign('meta_title', 'Progress');
-        $this->display();
-    }
-
-    public function condition()
-    {
-        $this->assign('meta_title', 'Condition');
-        $this->display();
-    }
-
-    // public function team()
-    // {
-    // 	$team_list = D('Team')->where(array('status' => 1))->limit(6)->order('id desc')->select();
-    //     $advisor_list = D('Advisor')->where(array('status' => 1))->limit(6)->order('id desc')->select();
-
-    //     $this->assign('team_list',$team_list);
-    //     $this->assign('advisor_list',$advisor_list);
-    //     $this->assign('meta_title', 'Team');
-    //     $this->display();
-    // }
 }
